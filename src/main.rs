1b6b78cc4e3a558a46745672e8e1bad1279e8b37
Rob Patro <rob.patro@gmail.com>
2024-07-07 11:46:21 -0400

    some progress

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,23 @@ fn sanitize_dependency_entry(ent: &mut toml_edit::Value) -> anyhow::Result<bool>
     Ok(did_remove)
 }
 
+fn get_crate_version(ent: &mut toml_edit::Value) -> anyhow::Result<String> {
+    let mut did_remove = false;
+    match ent {
+        Value::String(s) => Ok(s.to_string()),
+        Value::Integer(i) => Ok(i.to_string()),
+        Value::Float(f) => Ok(f.to_string()),
+        Value::Boolean(_b) => Err(anyhow!("cannot convert boolean to version string")),
+        Value::Datetime(_d) => Err(anyhow!("cannot convert datetime to version string")),
+        Value::Array(_a) => Err(anyhow!("cannont convert array to version string")),
+        Value::InlineTable(t) => Ok(format!(
+            "\"{}\"",
+            t.get("version").unwrap().as_str().unwrap()
+        )),
+    }
+}
+
+#[cfg(feature = "verify_crates")]
 fn crate_key(s: String) -> String {
     let mut pref = String::new();
     match s.len() {
@@ -50,6 +67,29 @@ fn crate_key(s: String) -> String {
         3 => format!("3/{}/{}", &s[0..1], s),
         4.. => format!("{}/{}/{}", &s[0..2], &s[2..4], s),
         _ => format!("ERROR"),
+    }
+}
+
+#[cfg(feature = "verify_crates")]
+fn get_crate_info_as_json(
+    key: String,
+    agent: &ureq::Agent,
+) -> anyhow::Result<Vec<serde_json::Value>> {
+    let mut body_res = String::from("[");
+    body_res.push_str(
+        &agent
+            .get(&format!("https://index.crates.io/{}", crate_key(key)))
+            .call()?
+            .into_string()?,
+    );
+    body_res = body_res.replace("\n", ",\n");
+    body_res.pop();
+    body_res.pop();
+    body_res.push(']');
+    match serde_json::from_str::<serde_json::Value>(&body_res) {
+        Ok(serde_json::Value::Array(a)) => Ok(a),
+        Ok(v) => Err(anyhow!("unexpected JSON types")),
+        Err(e) => Err(anyhow!("Invalid JSON : {:?}", e)),
     }
 }
 
@@ -69,30 +109,25 @@ fn sanitize(doc: &mut DocumentMut) -> anyhow::Result<()> {
                 }
                 toml_edit::Item::Value(v) => {
                     let did_remove = sanitize_dependency_entry(v)?;
-                    eprintln!("v : {:?}", v);
-                    if did_remove {
-                        eprintln!("{:#?}", _k.get());
-                    }
+
                     #[cfg(feature = "verify_crates")]
                     if did_remove {
-                        let body: String = agent
-                            .get(&format!(
-                                "https://index.crates.io/{}",
-                                crate_key(_k.to_string())
-                            ))
-                            .call()?
-                            .into_string()?;
-                        /*
-                        let r = client.get_crate(_k.get()).map_err(|e| {
-                            anyhow!(
-                                "could not verify the crate {} in the registry; error {:?}",
-                                _k.get(),
-                                e
-                            )
-                        })?;
-                        */
-                        eprintln!("body {}", body);
-                        //eprintln!("verified crate {}", r.crate_data.id);
+                        let ver = get_crate_version(v)?;
+                        let a = get_crate_info_as_json(_k.to_string(), &agent)?;
+                        for x in a.iter() {
+                            let fetched_ver = x.get("vers").unwrap().to_string();
+                            if ver == fetched_ver {
+                                eprintln!(
+                                    "observed_version: {}, fetched_version {}",
+                                    ver, fetched_ver
+                                );
+                            } else {
+                                eprintln!(
+                                    "observed_version: [{}] != fetched_version [{}]",
+                                    ver, fetched_ver
+                                );
+                            }
+                        }
                     }
                 }
                 toml_edit::Item::ArrayOfTables(_aot) => {
