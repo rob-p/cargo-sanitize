098c40e9f3d9235f7831df08fd2b76675f0b26c2
Rob Patro <rob.patro@gmail.com>
2024-07-07 00:14:45 -0400

    working on validate feature

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use anyhow::bail;
+use anyhow::{anyhow, bail};
 use clap::Parser;
 use std::io::prelude::*;
 use std::path::PathBuf;
 use toml_edit::{DocumentMut, Value};
+
+#[cfg(feature = "verify_crates")]
+use std::time::Duration;
+#[cfg(feature = "verify_crates")]
+use ureq::{Agent, AgentBuilder};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -17,7 +22,8 @@ struct Cli {
 
 const KEYS_TO_REMOVE: [&str; 5] = ["git", "tag", "branch", "rev", "path"];
 
-fn sanitize_dependency_entry(ent: &mut toml_edit::Value) -> anyhow::Result<()> {
+fn sanitize_dependency_entry(ent: &mut toml_edit::Value) -> anyhow::Result<bool> {
+    let mut did_remove = false;
     match ent {
         Value::String(_s) => { /* do nothing */ }
         Value::Integer(_i) => { /* do nothing */ }
@@ -27,14 +33,34 @@ fn sanitize_dependency_entry(ent: &mut toml_edit::Value) -> anyhow::Result<()> {
         Value::Array(_a) => { /* do nothing */ }
         Value::InlineTable(t) => {
             for k in KEYS_TO_REMOVE {
-                t.remove(k);
+                if let Some(_v) = t.remove(k) {
+                    did_remove = true;
+                }
             }
         }
     }
-    Ok(())
+    Ok(did_remove)
+}
+
+fn crate_key(s: String) -> String {
+    let mut pref = String::new();
+    match s.len() {
+        1 => format!("1/{}", s),
+        2 => format!("2/{}", s),
+        3 => format!("3/{}/{}", &s[0..1], s),
+        4.. => format!("{}/{}/{}", &s[0..2], &s[2..4], s),
+        _ => format!("ERROR"),
+    }
 }
 
 fn sanitize(doc: &mut DocumentMut) -> anyhow::Result<()> {
+    #[cfg(feature = "verify_crates")]
+    // Instantiate the client.
+    let agent = ureq::AgentBuilder::new()
+        .timeout_read(Duration::from_secs(5))
+        .timeout_write(Duration::from_secs(5))
+        .build();
+
     if let Some(ref mut deps) = &mut doc["dependencies"].as_table_like_mut() {
         for (_k, item) in deps.iter_mut() {
             match item {
@@ -42,7 +68,32 @@ fn sanitize(doc: &mut DocumentMut) -> anyhow::Result<()> {
                     bail!("unexpected format of dependency table");
                 }
                 toml_edit::Item::Value(v) => {
-                    sanitize_dependency_entry(v)?;
+                    let did_remove = sanitize_dependency_entry(v)?;
+                    eprintln!("v : {:?}", v);
+                    if did_remove {
+                        eprintln!("{:#?}", _k.get());
+                    }
+                    #[cfg(feature = "verify_crates")]
+                    if did_remove {
+                        let body: String = agent
+                            .get(&format!(
+                                "https://index.crates.io/{}",
+                                crate_key(_k.to_string())
+                            ))
+                            .call()?
+                            .into_string()?;
+                        /*
+                        let r = client.get_crate(_k.get()).map_err(|e| {
+                            anyhow!(
+                                "could not verify the crate {} in the registry; error {:?}",
+                                _k.get(),
+                                e
+                            )
+                        })?;
+                        */
+                        eprintln!("body {}", body);
+                        //eprintln!("verified crate {}", r.crate_data.id);
+                    }
                 }
                 toml_edit::Item::ArrayOfTables(_aot) => {
                     bail!("unexpected format of dependency table");
